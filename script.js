5710115be273cd620ac214d7a93f38a60ff1a7c6
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-07-04 22:32:42 +0300

    check score with Timing Events

--- script.js
+++ script.js
@@ -34,13 +34,61 @@ let equationObject = {};
 const wrongFormat = [];
 
 // Time
+let timer;
+let timePlayed = 0;
+let baseTime = 0;
+let penaltyTime = 0;
+let finalTime = 0;
+let finalTimeDisplay = '0.0s';
+
+
 
 // Scroll
 let valueY = 0;
 
+// Stop Timer, Process Results,  go to Score Page
+const checkTime = () => {
+  console.log(timePlayed);
+  if (playerGuessArray.length == questionAmount) {
+    console.log('player guess array:', playerGuessArray)
+    clearInterval(timer);
+    // Check for wrong guesses, add penalty time
+    equationsArray.forEach((equation, index) => {
+      if (equation.evaluated === playerGuessArray[index]) {
+        //Correct Guess, No Penalty
+      } else {
+        // Incorrect Guess, Add Penalty
+        penaltyTime += 0.5;
+      }
+    });
+    finalTime = timePlayed + penaltyTime;
+    console.log('time', timePlayed, 'penalty:', penaltyTime, 'final:', finalTime)
+  }
+}
+
+
+//Add a tenth of a second to timePlayed
+
+const addTime = () => {
+  timePlayed += 0.1;
+  checkTime();
+
+}
+
+
+//Start timer when game page is clicked
+
+const startTimer = () =>{
+  // Reset times
+  timePlayed = 0;
+  penaltyTime = 0;
+  finalTime = 0;
+  timer = setInterval(addTime, 100);
+  gamePage.removeEventListener('click', startTimer);
+}
+
 // Scroll, Store user selection in playerGuessArray
 const select=(guessedTrue) => {
-  console.log('player guess array:', playerGuessArray)
   // Scroll 80 pixels
   valueY += 80;
   itemContainer.scroll(0, valueY);
@@ -169,3 +217,4 @@ startForm.addEventListener('click', () => {
 
 // Event listeners
 startForm.addEventListener('submit', selectQuestionAmount);
+gamePage.addEventListener('click', startTimer);
