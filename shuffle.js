656a243c4c96e73b1d5d3ca3b2cb6b719bea1d5a
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-07-05 19:11:31 +0300

    deposit scores in Local Storage

--- shuffle.js
+++ shuffle.js
@@ -1,17 +1,19 @@
 function shuffle(array) {
-       let currentIndex = array.length;
-     
-       // While there remain elements to shuffle...
-       while (currentIndex != 0) {
-     
-         // Pick a remaining element...
-         let randomIndex = Math.floor(Math.random() * currentIndex);
-         currentIndex--;
-     
-         // And swap it with the current element.
-         [array[currentIndex], array[randomIndex]] = [
-           array[randomIndex], array[currentIndex]];
-       }
-       return array;
-     }
-     
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
